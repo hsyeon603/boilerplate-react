ef905c587bc65d71da673f9087532166cc03f982
hsyeon603 <hsyeon603@gmail.com>
2024-04-17 22:35:20 +0900

    ✨ Implement mixins for flexbox

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,11 @@
 import { responsive } from './mixins/responsive';
 import { position } from './mixins/position';
+import { flexbox } from './mixins/flexbox';
 
 const theme = {
   responsive,
   position,
+  flexbox,
 };
 
 export default theme;
