34cf4460592c4f40fb03a59f30c45ed3408b7f77
hsyeon603 <hsyeon603@gmail.com>
2024-04-17 16:06:15 +0900

    💄 Declare color constants

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,12 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 import normalize from 'styled-normalize';
 import reset from 'styled-reset';
-import { fonts } from './fonts';
+import { font } from './font';
+import { color } from './color';
 
 const GlobalStyle = createGlobalStyle`
     ${normalize}
     ${reset}
-    ${fonts}
+    ${font}
+    ${color}
+
+    span {
+        font-family: 'Noto Sans KR', 'Roboto';
+        font-weight: 400;
+        color: var(--color-blue);
+    }
 `;
 
 export default GlobalStyle;
---
+++ src/styles/color.ts
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+
+export const color = css`
+  :root {
+    --color-black: #000;
+    --color-dark: #191a20;
+    --color-primary: #3f4150;
+    --color-secondary: #8c8d96;
+    --color-tertiary: #b2b3b9;
+    --color-border: #e0e2e7;
+    --color-background: #f7f8fa;
+    --color-white: #fff;
+    --color-blue: #3da5f5;
+  }
+`;
