b29ed4c7c19a5c5d0cb6c6bc20387435965cda6b
hsyeon603 <hsyeon603@gmail.com>
2024-04-17 22:37:11 +0900

    🩹 Fix position mixin's parameter type

--- src/styles/mixins/position.ts
+++ src/styles/mixins/position.ts
@@ -1,15 +1,17 @@
+type PositionValue = 'absolute' | 'fixed';
+
 export const position = {
-  centerX: (type: 'absolute') => `
+  centerX: (type: PositionValue = 'absolute') => `
     position: ${type};
     left: 50%;
     transform: translateX(-50%);
 	`,
-  centerY: (type: 'absolute') => `
+  centerY: (type: PositionValue = 'absolute') => `
 		position: ${type};
 		top: 50%;
 		transform: translateY(-50%);
 	`,
-  center: (type: 'absolute') => `
+  center: (type: PositionValue = 'absolute') => `
 		position: ${type};
 		left: 50%;
 		top: 50%;
